1e628f165370de6b0faf09996abc4ba2bfe845ee
ahmedelfakire <132910110+ahmedelfakire@users.noreply.github.com>
2026-01-01 13:39:13 +0100

    Remove toggle for deploy

--- saas-landing-demo/src/app/page.tsx
+++ saas-landing-demo/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { ModeToggle } from "@/components/mode-toggle";
+
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
